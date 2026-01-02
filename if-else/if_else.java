1dd9f417cf04c8eb02c94880a4b20673508ff6ec
Himanshu <himanshuchaudhery962@gmail.com>
2026-01-02 22:24:27 +0530

    If-Else Commit

---
+++ if-else/if_else.java
@@ -0,0 +1,12 @@
+public class if_else{
+    public static void main(String []args){
+        int a=10;
+        int b=12;
+        if(a<b){
+            System.out.println("B is greater than A");
+        }
+        else{
+            System.out.println("A is greater than B");
+        }
+    }
+}
