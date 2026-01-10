db49e94b22290d0b139064efafce181f24aae4ab
Himanshu <himanshuchaudhery962@gmail.com>
2026-01-10 21:51:23 +0530

    Commiting While Loop

---
+++ Whileloop/whileLoop.java
@@ -0,0 +1,9 @@
+public class whileLoop{
+    public static void main(String[] args){
+        int a = 10;
+        while(a!=0){
+            System.out.println(a);
+            a=a-1;
+        }
+    }
+}
